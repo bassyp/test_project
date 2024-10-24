4b0961bdee4a88d7e852e266b80e4fdebb0e5430
Robert R. <rassaur@yahoo.de>
2024-10-24 20:07:13 +0200

    init project

---
+++ rrbbs/tailwind.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  "content": [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js"
+  ],
+  "plugins": [
+    require('daisyui')
+  ],
+  "daisyui": {
+    "themes": [
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset"
+    ]
+  }
+}
